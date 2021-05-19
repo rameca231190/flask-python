60b0851e2fcdf8044ff14ac326ce67d5d070f74d
rameca231190 <romapereverziev@gmail.com>
2021-05-18 20:22:05 -0400

    scripts

--- app/app.py
+++ app/app.py
@@ -1,8 +1,28 @@
 from flask import Flask
+from flask import render_template
+#from app import app
+
 app = Flask(__name__)
+
+#def home():
+#  return "Welcome to my app!"
+
 @app.route("/")
-def home():
-  return "Welcome to my app!"
+@app.route('/index')
+def index():
+    user = {'username': 'Roman'}
+    posts = [
+        {
+            'author': {'username': 'Nastia'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
